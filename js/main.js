0955f384c6e1c1efd38985f2fa396586465e420d
Qichun Dai <49536404+qichun-dai@users.noreply.github.com>
2023-07-07 18:31:19 +0200

    update d3 links

--- js/main.js
+++ js/main.js
@@ -1,18 +1,14 @@
 async function drawForce() {
-    // 1. Access data
+    // read data
     const dataset = await d3.json("./data/harry_potter.json")
     console.log(dataset)
 
     const nodes = dataset.nodes
     const links = dataset.links
 
-    const simulation = d3.forceSimulation(nodes)
-    .force("charge", d3.forceManyBody())
-    .force("link", d3.forceLink(links))
-    .force("center", d3.forceCenter());
+    console.log(dataset.nodes)
+    console.log(dataset.links)
 
-
-    
     }
     
     drawForce()
